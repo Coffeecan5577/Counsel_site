0421ba8efe1358ebd49a05b624c94f32c07812d5
Coffeecan <c.curry2210@gmail.com>
2017-08-10 14:27:21 -0500

    Making more progress and changes. Work on the card deck section is underway.

---
+++ js/scroll.js
@@ -0,0 +1,7 @@
+/* Code to scroll at the top and bottom of page:
+
+$(document).ready(function () {
+$('#selector').click(function () { $('html, body').animate({ scrollTop: $(document).height() }, 1000); }); });
+
+Add something to scroll back to top of page, and change the Navbar text to say "Scroll to bottom and scroll to top resepctively at each section."
+*/
