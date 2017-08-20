1f8cc9f9c2754603dbb9911429a31b786eb9b398
Coffeecan <c.curry2210@gmail.com>
2017-08-19 20:43:54 -0500

    Made some more changes to file references. Still need to get all file references fixed.

--- php/index.php
+++ php/index.php
@@ -1,7 +1,242 @@
-<?php
-  // Included files will go here:
-  include ("navbar.php");
-  include ("header.php");
-  include ("footer.php");
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+   <!-- Required meta tags -->
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
- ?>
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+   <link rel="stylesheet" href="public_html/css/counsel.css" />
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+   <link href="https://fonts.googleapis.com/css?family=Raleway:400,400i" rel="stylesheet"> <!--Font reference -->
+ </head>
+
+
+ <body data-spy="scroll" data-target="#navbar" data-offset="150">
+   <!--First things first, navbar-->
+   <nav class="navbar navbar-toggleable-md fixed-top navbar-inverse bg-inverse">
+   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
+   </button>
+   <a class="navbar-brand" href="#">Back to top</a>
+
+   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+     <ul class="navbar-nav mr-auto">
+       <li class="nav-item" name="services">
+         <a class="nav-link" href="#service_link" id="service_tab">Services</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="#about_link">About</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="#contact_link">Contact</a>
+       </li>
+     </ul>
+   </div>
+ </nav>
+
+     <!--Link to top of home page -->
+      <p id="top"></p>
+
+    <!--Jumbotron next-->
+    <div class="container-fluid">
+      <div class="jumbotron" id="jumbotron_intro">
+         <div id="jumbo_text">
+            <h1 class="display-3">Darlene Curry, M.Ed., M.A., L.P.C.</h1>
+            <h2 class="dislay-3">Licensed Professional Counselor</h2>
+            <hr />
+            <hr />
+            <div id="sub_heading_text">
+               <h2>Helping others find their way...</h2>
+               <hr />
+               <h3 class="fade_text">Currently accepting clients!</h3>
+            </div>
+         </div>
+      </div>
+   </div>
+    <!--Next up is the card deck section, listing services-->
+    <!--paragraph ID first for scrolling and navbar purposes -->
+    <p id="service_link"></p>
+    <div class="container-fluid">
+      <div class="jumbotron" id="jumbotron_crd_services">
+         <div class="card-deck-wrapper">
+            <div class="card-deck">
+              <div class="card">
+                <div class="card-block special-card">
+                  <h2 class="card-title">Services</h2>
+                  <p class="card-text">I'm a licensed professional counselor with over 10 years of experience helping children, adolescents, and adults work through some of life's challenging situations.  I strive  to help in a variety of areas:  depression, anxiety, self-harm, grief, marital issues, communication, pre-marital counseling, life coaching, and post-secondary readiness. </p>
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-block special-card" >
+                  <h2 class="card-title">Types of Therapies</h2>
+                  <p class="card-text">Each client and each situation is unique; therefore, it is best to develop a therapeutic plan that fits the client and the presenting issue.  Some of these therapies might include: cognitive therapy, reality based therapy,  and solution-based therapy.  A variety of resources are utilized to develop a comprehensive treatment plan.</p>
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-block special-card">
+                  <h2 class="card-title">Clients</h2>
+                  <p class="card-text"><span><h2>Children/Adolescents</h2></span></p> <!--Fix up the list items to align left with the bullet. do to both cards -->
+                  <ul class="service-list">
+                     <li>
+                        Developmental issues
+                     </li>
+                     <li>
+                        Depression
+                     </li>
+                     <li>
+                        Anxiety
+                     </li>
+                     <li>
+                        Grief
+                     </li>
+                     <li>
+                        Self-Esteem
+                     </li>
+                     <li>
+                        Executive Function Deficit
+                     </li>
+                     <li>
+                        ADD/ADHD Screening
+                     </li>
+                     <li>
+                        Post-Secondary Counseling
+                     </li>
+                     <li>
+                        Coping with Divorce
+                     </li>
+                     <li>
+                        Trauma
+                     </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-block special-card">
+                  <h2 class="card-title">Clients (continued)</h2>
+                  <p class="card-text"><span><h2>Adults</h2></span></p>
+                  <ul class="service-list">
+                     <li>
+                        Pre-Marital Counseling
+                     </li>
+                     <li>
+                        Communication
+                     </li>
+                     <li>
+                        Depression
+                     </li>
+                     <li>
+                        Anxiety
+                     </li>
+                     <li>
+                        Life-Mapping
+                     </li>
+                     <li>
+                        Marriage Counseling
+                     </li>
+                     <li>
+                        Vocational Counseling
+                     </li>
+                     <li>
+                        Grief
+                     </li>
+                     <li>
+                        Coping with Divorce
+                     </li>
+                     <li>
+                        Family Systems
+                     </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+         </div>
+      </div>
+    </div>
+
+
+    <!--Next is the About section -->
+    <p id="about_link"></p>
+    <div class="container">
+      <h1 id="h1_about">About Me</h1>
+      <div class="row">
+         <img class="rounded-circle mx-auto img-thumbnail" src="public_html/pics/darlene_pic_crop.jpg" name="profile_pic" />
+      </div>
+      <hr />
+      <div class="row">
+         <p id="p_about">
+            I'm Darlene M. Curry, M.Ed., M.A., L.P.C.  I've had the opportunity to live in the panhandle of Texas for twenty plus years.  In that time I have taught high school English, served as District Counselor, served as an Elementary Principal, and my greatest joy--served as a licensed counselor.  My education includes a Bachelor of Science degree from Wayland Baptist University, a Master's In Education from Northwestern Oklahoma State University, and a Master's in Counseling from Wayland Baptist University.  I am licensed with the State Board of Professional Counselors and I have enjoyed serving the panhandle for the last ten years.  Helping people find their way is PURE JOY for me!  I love working with people of all ages.​
+My counseling office is located in Gruver, TX...a great little community with fantastic people.  I look forward to helping whoever I can as soon as I can!  If you have a need that is outside of my level of expertise I will work diligently to find someone that can give the help that is needed.  I've always enjoyed working one on one with people to help them solve problems.  My compassionate, therapeutic approach sets me apart from many others you will encounter.  My wealth of experience in and around various facets of education also sets me apart from other counselors.  Helping others find their way is a tremendous blessing and my heart's desire is to be a help to those who are trying to find solutions on life's journey.
+         </p>
+      </div>
+    </div>
+
+
+
+   <!-- Now for the contact section, and the forms -->
+   <div class="container-fluid">
+      <div class="jumbotron" id="jumbotron_contact">
+         <h1 id="h1_contact">Contact Me</h1>
+         <hr />
+         <hr />
+         <div class="row">
+            <div class="col-md-4">
+               <iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBCWVGWrR4Te-usNr1DnbopLBpi_fiBxCo&q=430+Main+Ave" allowfullscreen></iframe>
+            </div>
+            <div class="col-md-4">
+               <h2 id="contact_info_1">Darlene M. Curry, M.Ed., M.A., L.P.C.</h2>
+               <hr />
+               <h2 id="contact_info_2">430 Main Street, Ste. A</h2>
+               <hr />
+               <h2 id="contact_info_3">Gruver, TX  79040</h2>
+               <hr />
+               <h2 id="contact_info_4">806-736-0463</h2>
+               <hr />
+            </div>
+
+            <div class="col-md-4">
+               <form class="form-inline"  id="ajax-contact" method="POST" action="mailer.php">
+                 <div class="form-group">
+                   <input type="text" class="form-control" id="inputName" placeholder="Enter name" required>
+                 </div>
+                 <div class="form-group">
+                   <input type="text" class="form-control" id="inputPhone" placeholder="Enter phone" required>
+                 </div>
+                 <div class="form-group">
+                   <textarea class="form-control" id="inputMessage" maxlength="100" placeholder="Enter message"></textarea>"
+                 </div>
+                 <button type="submit" class="btn btn-primary" id="btn_submit_info">Send Info</button>
+              </form>
+            </div>
+         </div>
+      </div>
+   </div>
+
+   <p id="contact_link"></p>
+
+   <!--Now for the footer -->
+   <div class="container-fluid"> <!--include facebook link <href="" -->
+      <footer id="counsel_footer"> &copy; 2017 Created by Cale Curry using Bootstrap 4<a href="https://www.facebook.com/profile.php?id=100015351240038" target="blank"><i class=" icon-lock fa fa-facebook-square fa-3x text-right" id="facebook_link"></i></a></footer>
+   </div>
+
+    <!--Finally our music: -->
+    <audio autoplay loop src="public_html/music_files/RachmaninovRhapsodyonaThemebyPaganini.mp3"></audio>
+
+
+   <!-- jQuery first, then Tether, then Bootstrap JS. -->
+   <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+   <script type="text/javascript" src="http://creativecouple.github.com/jquery-timing/jquery-timing.min.js"></script>
+   <!--FontAwesome embed link -->
+   <script src="https://use.fontawesome.com/e43661adf2.js"></script>
+   <script src="public_html/js/fade_trans.js"></script>
+   <script src="public_html/js/scroll.js"></script>
+ </body>
+</html>
+
+<?php
+   include ("public_html/html/mailer.php");
+?>
